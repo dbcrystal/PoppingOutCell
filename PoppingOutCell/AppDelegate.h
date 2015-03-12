0782a54b5a788f2da32c246ec5cb009dd081b09d
dbcrystal <dbcrystallan@gmail.com>
2015-03-12 10:17:59 +0800

    Initial Commit

---
+++ PoppingOutCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PoppingOutCell
+//
+//  Created by LAN on 3/12/15.
+//  Copyright (c) 2015 LAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PoppingOutCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PoppingOutCell
+//
+//  Created by LAN on 3/12/15.
+//  Copyright (c) 2015 LAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
